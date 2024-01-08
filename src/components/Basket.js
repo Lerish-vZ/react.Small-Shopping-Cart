ed3b18d373eaa250ab3909101adb16737f29768a
Lerish-vZ <lerishavz@gmail.com>
2024-01-08 15:10:37 +0200

    Update

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function Basket(props){
+    const {cartItems} = props;
     return (
         <aside className='block col-1'>
             <h2> Cart Items </h2>
