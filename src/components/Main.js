51294768d9ac85a333729ffb6eda456e54eecbab
Lerish-vZ <lerishavz@gmail.com>
2024-01-08 14:25:29 +0200

    Update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React from "react";
 
-export default function Main(props){
-    return <div> Main </div>
+export default function Main(props) {
+  return (
+    <main className="block col-2">
+      <h2> Products </h2>
+      <div className="row"></div>
+    </main>
+  );
 }
