618f75fc71d8dbe0e7bfa3ba032145d73a42295a
Lerish-vZ <lerishavz@gmail.com>
2024-01-08 14:15:29 +0200

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,10 @@ export default function Header(props){
                     <h1> Small Shopping Cart </h1>
                 </a>
             </div>
+            <div>
+                <a href='#/cart'> Cart </a>
+                <a href='#/signin'> SignIn </a>
+            </div>
         </header>
     )
 }
