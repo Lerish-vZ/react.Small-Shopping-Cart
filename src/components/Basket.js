03c66f060f51b881e87db43c5215cd38e8919eb9
Lerish-vZ <lerishavz@gmail.com>
2024-01-08 15:51:23 +0200

    Update

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default function Basket(props) {
+
   const { cartItems, onAdd, onRemove } = props;
+
   const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
   const taxPrice = itemsPrice * 0.14;
   const shippingPrice = itemsPrice > 2000 ? 0 : 20;
   const totalPrice = itemsPrice + taxPrice + shippingPrice;
+  
   return (
     <aside className="block col-1">
       <h2>Cart Items</h2>
@@ -17,7 +20,7 @@ export default function Basket(props) {
             <div className="col-2">
               <button onClick={() => onRemove(item)} className="remove">
                 -
-              </button>{' '}
+              </button>{" "}
               <button onClick={() => onAdd(item)} className="add">
                 +
               </button>
@@ -57,7 +60,7 @@ export default function Basket(props) {
             </div>
             <hr />
             <div className="row">
-              <button onClick={() => alert('Implement Checkout!')}>
+              <button onClick={() => alert("Implement Checkout!")}>
                 Checkout
               </button>
             </div>
