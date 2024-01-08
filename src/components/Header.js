4242221393b320876cb571f321f8ec4c3eccb686
Lerish-vZ <lerishavz@gmail.com>
2024-01-08 15:51:53 +0200

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from "react";
 
-export default function Header(props){
-    const {countCartItems} = props;
-    return(
-        <header className='row block center'>
-            <div>
-                <a href='#/'>
-                    <h1> Small Shopping Cart </h1>
-                </a>
-            </div>
-            <div>
-                <a href='#/cart'>
-                     Cart {' '}
-                     {countCartItems ? (
-                        <button className='badge'>{countCartItems}</button>
-                     ) : (
-                        ''
-                     )}
-                     </a>{' '} 
-                     <a href='#/signin'> SignIn </a>
-            </div>
-        </header>
-    )
+export default function Header(props) {
+  return (
+    <header className="block row center">
+      <div>
+        <a href="#/">
+          <h1>Small Shopping Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">
+          Cart{" "}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ""
+          )}
+        </a>{" "}
+        <a href="#/signin"> SignIn</a>
+      </div>
+    </header>
+  );
 }
