4996f2264a006fd2e363abd9ef7c7dd9bb78157b
Lerish-vZ <lerishavz@gmail.com>
2024-01-08 14:48:44 +0200

    Update

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Mackbook',
+            price: 2000,
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.gottabemobile.com%2Fwp-content%2Fuploads%2F2013%2F12%2F13-inch-MacBook-Pro-Retina-Review-Late-2013-004.jpg&f=1&nofb=1&ipt=53739eceb6cdd91947f150b6f679c9238cccb4ee9721d369b6d0be2966cf09f1&ipo=images'
+        },
+
+        {
+            id: '2',
+            name: 'AirPods',
+            price: 1500,
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstore.storeimages.cdn-apple.com%2F4982%2Fas-images.apple.com%2Fis%2FMWP22%3Fwid%3D1144%26hei%3D1144%26fmt%3Djpeg%26qlt%3D80%26op_usm%3D0.5%2C0.5%26.v%3D1572990352299&f=1&nofb=1&ipt=13087a55e1710d4d19c4cbd1f8e44bb96d0521002e37fdae078a01e6ab784ae2&ipo=images'
+        },
+
+        {
+            id: '3',
+            name: 'iMac Keyboard',
+            price: 1000,
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F5.imimg.com%2Fdata5%2FVX%2FDR%2FMY-41488230%2Fapple-keyboard-500x500.jpg&f=1&nofb=1&ipt=b3d76c98ec77189d2e5db6c6626be5bd61a55dd908617216aa6e3e4044e70ee8&ipo=images'
+        }
+    ]
+};
+
+export default data;
